0dd8e36e87f289543bf8f01c9ae82ca3c431fda8
Maxime <elessamaxime@icloud.com>
2021-08-18 15:00:21 +0100

    [MAJ : modification de l'administration]

--- assets/admin.js
+++ assets/admin.js
@@ -4,6 +4,7 @@ import './adminJs/adminlte.min';
 import './adminJs/demo';
 import 'jquery';
 import '@fortawesome/fontawesome-free/js/all';
+import './adminJs/jquery.overlayScrollbars.min';
 $(document).ready(function () {
     $('.product-image-thumb').on('click', function () {
         var $image_element = $(this).find('img')
